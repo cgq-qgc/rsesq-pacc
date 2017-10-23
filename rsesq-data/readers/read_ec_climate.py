1d1e64e34a74a67b196583ae32191d1376976780
Jean-Sébastien Gosselin <jnsebgosselin@gmail.com>
2017-10-23 15:53:34 -0400

    First commit

---
+++ rsesq-data/readers/read_ec_climate.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 23 15:28:06 2017
+@author: jsgosselin
+"""
+
+# ---- Imports: standard library
+
+from urllib.request import urlopen
+from urllib.error import HTTPError, URLError
+
+# ---- Imports: local
+
+from readers.base import AbstractReader
+
+
+# ---- Base functions
+
+
+def get_climate_stations_from_tor():
+    """
+    Get the list of available climate station from Tor ftp.
+    """
+    # Save the content of the file in a memory buffer.
+    url = "ftp://client_climate@ftp.tor.ec.gc.ca"
+    url += "/Pub/Get_More_Data_Plus_de_donnees/Station%20Inventory%20EN.csv"
+
+    try:
+        html = urlopen(url).read()
+    except (HTTPError, URLError):
+        return None
+
+    try:
+        data = html.decode('utf-8-sig').split('\n')
+        return data
+    except (UnicodeDecodeError, UnicodeError):
+        return None
+
+if __name__ == "__main__":
+    data = get_climate_stations_from_tor()
+    
+    # reader = EC_Climate_Reader()
+    # reader.save_station_to_csv('01160002', 'test.csv')
