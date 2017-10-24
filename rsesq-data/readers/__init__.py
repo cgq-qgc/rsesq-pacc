400c127a47a9fce49307d8d89858a84c0b2285b1
Jean-Sébastien Gosselin <jnsebgosselin@gmail.com>
2017-10-24 14:52:34 -0400

    Added to module __init__ and Notebook

--- rsesq-data/readers/__init__.py
+++ rsesq-data/readers/__init__.py
@@ -6,3 +6,4 @@ Created on Sat Apr  8 13:56:17 2017
 
 from readers.read_mddelcc_cehq import MDDELCC_CEHQ_Reader
 from readers.read_mddelcc_rses import MDDELCC_RSESQ_Reader
+from readers.read_ec_climate import EC_Climate_Reader
