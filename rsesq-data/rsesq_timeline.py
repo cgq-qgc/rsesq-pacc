6d868f53cc150e6272aaa023f83c4bbc3e670593
Jean-Sébastien Gosselin <jnsebgosselin@gmail.com>
2017-10-25 13:50:16 -0400

    Code to produce the graph

--- rsesq-data/rsesq_timeline.py
+++ rsesq-data/rsesq_timeline.py
@@ -36,3 +36,36 @@ def compute_nbr_stn_bins(bstep):
 
     return years, values
 
+
+# ---- Produce the plot
+
+# Produce the figure.
+plt.close('all')
+fig, ax = plt.subplots(figsize=(8, 5))
+ax.set_facecolor('0.85')
+ax.grid(color='white', linestyle='-', linewidth=1)
+ax.set_axisbelow(True)
+for loc in ax.spines:
+    ax.spines[loc].set_visible(False)
+ax.tick_params(axis='both', which='both', length=0)
+
+# Produce the barplot.
+years, values = compute_nbr_stn_bins(5)
+ax.bar(years[:-1]+2.5, values, 4)
+ax.set_ylabel('Nombre de stations actives', fontsize=16, labelpad=10)
+ax.set_xlabel('Années', fontsize=16, labelpad=10)
+ax.set_xticks(np.append(years, years[-1]+5))
+
+# Plot text values over the barplot.
+for year, value in zip(years[:-1], values):
+    ax.text(year+2.5, value+1, str(value), ha='center', va='bottom')
+
+# Plot a continous line.
+years, values = compute_nbr_stn_bins(1)
+ax.plot(years[:-1], values, ls='--', lw=1.5, color='red',
+        dashes=[5, 2], alpha=0.75)
+
+fig.savefig('nbr_stns_actives_vs_temps.pdf')
+plt.tight_layout()
+plt.show(block=False)
+fig.savefig('nbr_stns_actives_vs_temps.pdf')
