487e1f0f180089381b05e5eeb568ccba796ed737
Jean-Sébastien Gosselin <jnsebgosselin@gmail.com>
2017-10-25 13:45:29 -0400

    Code to produce bins from the database

--- rsesq-data/rsesq_timeline.py
+++ rsesq-data/rsesq_timeline.py
@@ -4,4 +4,35 @@ Created on Wed Oct 25 08:59:57 2017
 @author: jsgosselin
 """
 
-from 
+import numpy as np
+import matplotlib.pyplot as plt
+from readers import MDDELCC_RSESQ_Reader
+
+
+# ---- Produce the bins
+
+def compute_nbr_stn_bins(bstep):
+    reader = MDDELCC_RSESQ_Reader()
+    bins = {}
+    for stn in reader.stations():
+        if 'Year' not in list(stn.keys()):
+            continue
+
+        dec = np.unique(stn['Year']//bstep)*bstep
+        for d in dec.astype(str):
+            if d not in list(bins.keys()):
+                bins[d] = 0
+            else:
+                bins[d] = bins[d] + 1
+
+    years = np.array(list(bins.keys())).astype(int)
+    values = np.array(list(bins.values())).astype(int)
+
+    indexes = np.argsort(years)
+    years = years[indexes]
+    values = values[indexes]
+
+    years = np.append(years, years[-1]+bstep)
+
+    return years, values
+
