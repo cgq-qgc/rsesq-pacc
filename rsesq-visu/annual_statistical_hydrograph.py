5bbe6f2e808905b1b3e55bc4ea49d2ecc29c99e5
Jean-Sébastien Gosselin <jnsebgosselin@gmail.com>
2017-10-27 09:01:10 -0400

    Code to plot statistical annual hydrograph

--- rsesq-visu/annual_statistical_hydrograph.py
+++ rsesq-visu/annual_statistical_hydrograph.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Oct 25 20:24:09 2017
-
 @author: jsgosselin
 """
 
-rbg = [(57, 82, 164), (110, 205, 222), (105, 189, 69), (248, 151, 29),
-       (152, 100, 38)]import os
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.transforms as transforms
 from readers import MDDELCC_RSESQ_Reader
+
+rbg = [[57/255, 82/255, 164/255],
+       [110/255, 205/255, 222/255],
+       [105/255, 189/255, 69/255],
+       [248/255, 151/255, 29/255],
+       [152/255, 100/255, 38/255]]
 
 
 def compute_monthly_statistics_table(years, months, values, q):
@@ -20,3 +24,68 @@ def compute_monthly_statistics_table(years, months, values, q):
         percentiles.append(np.percentile(values[ixs], q))
     return np.array(percentiles)
 
+
+def plot_10yrs_annual_statistical_hydrograph(sid, cur_year):
+    reader = MDDELCC_RSESQ_Reader()
+    stn_data = reader._db[sid]
+
+    # Generate the percentiles.
+    year = stn_data['Year']
+    month = stn_data['Month']
+    level = stn_data['Elevation'] - stn_data['Water Level']
+    q = [100, 90, 75, 50, 25, 10, 0]
+    percentiles = compute_monthly_statistics_table(year, month, level, q)
+
+    # Produce the figure.
+    fig, ax = plt.subplots(figsize=(8, 5))
+    ax.set_facecolor('0.85')
+    ax.grid(axis='both', color='white', linestyle='-', linewidth=1)
+    ax.set_axisbelow(True)
+    ax.invert_yaxis()
+    for loc in ax.spines:
+        ax.spines[loc].set_visible(False)
+    ax.tick_params(axis='both', which='both', length=0)
+
+    # Plot the percentiles.
+    xpos = range(12)
+    idx = [0, 1, 2, 4, 5, 6]
+    for i in range(len(idx)-1):
+        ax.bar(xpos, percentiles[:, idx[i]]-percentiles[:, idx[i+1]], 0.95,
+               percentiles[:, idx[i+1]], color=rbg[i])
+    ax.plot(xpos, percentiles[:, 3], '^k')
+
+    # Plot daily series.
+    year = stn_data['Year']
+    time = stn_data['Time'][year == cur_year]
+    level = stn_data['Elevation'] - stn_data['Water Level'][year == cur_year]
+    ax.plot((time-time[0])/365*12-0.5, level, '.', color='red')
+
+    # Set axis labels.
+    ax.set_ylabel("Niveau d'eau en m sous la surface", fontsize=16,
+                  labelpad=10)
+
+    # Set ticks and ticklabels.
+    ax.set_xticks(np.arange(-0.5, 11.51))
+    ax.set_xticks(np.arange(12), minor=True)
+
+    months = ['Jan', 'Fév', 'Mar', 'Avr', 'Mai', 'Jun', 'Jui', 'Aoû', 'Sep',
+              'Oct', 'Nov', 'Déc']
+    ax.set_xticklabels([])
+    ax.set_xticklabels(months, minor=True, fontsize=12)
+
+    # Add a Title.
+    offset = transforms.ScaledTranslation(0, 3/72, fig.dpi_scale_trans)
+    title = "Puits %s (%s)" % (stn_data['Name'], sid)
+    ax.text(0, 1, title, weight='bold', fontsize=12,
+            transform=ax.transAxes+offset)
+
+    # Plot and save figure.
+    plt.tight_layout()
+    plt.show(block=False)
+
+    return percentiles
+
+
+if __name__ == "__main__":
+    plt.close('all')
+    percentiles = plot_10yrs_annual_statistical_hydrograph('03090006', 2016)
