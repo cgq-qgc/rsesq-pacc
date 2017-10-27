0ce6a85e3cac658f1bf90b2ff49256b32c060c2b
Jean-Sébastien Gosselin <jnsebgosselin@gmail.com>
2017-10-27 09:00:48 -0400

    Code to compute monthly percentiles

--- rsesq-visu/annual_statistical_hydrograph.py
+++ rsesq-visu/annual_statistical_hydrograph.py
@@ -6,4 +6,17 @@ Created on Wed Oct 25 20:24:09 2017
 """
 
 rbg = [(57, 82, 164), (110, 205, 222), (105, 189, 69), (248, 151, 29),
-       (152, 100, 38)]
+       (152, 100, 38)]import os
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.transforms as transforms
+from readers import MDDELCC_RSESQ_Reader
+
+
+def compute_monthly_statistics_table(years, months, values, q):
+    percentiles = []
+    for m in range(1, 13):
+        ixs = np.where(months == m)[0]
+        percentiles.append(np.percentile(values[ixs], q))
+    return np.array(percentiles)
+
