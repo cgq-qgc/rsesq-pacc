348db5820d9a849f0306d6c0a1f2774c44bdd8ba
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2019-06-18 19:44:04 -0400

    Add init to correction_niveaux

---
+++ correction_niveaux/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © Jean-Sébastien Gosselin
+# https://github.com/cgq-qgc/pacc-inrs
+#
+# Licensed under the terms of the MIT License.
+# -----------------------------------------------------------------------------
