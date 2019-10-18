c2c7cdb3471670878288c394b050619f9bbc5766
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2019-10-18 11:17:27 -0400

    Make routine more robust

--- correction_niveaux/correct_waterlevels.py
+++ correction_niveaux/correct_waterlevels.py
@@ -68,109 +68,94 @@ for sid in rsesq_reader.station_ids():
                          'gwt_correction_baro',
                          'brf_results',
                          'brf_{}.csv'.format(sid))
-    if not osp.exists(brf_fname):
-        # This means that no BRF is available for this probably, probably
-        # because it is influenced by pumping.
+
+    if sid not in influenced and not osp.exists(brf_fname):
+        # This means that no BRF analysis has been done yet for that well.
         continue
-
     iwell += 1
-    print("{:>3d} - Correcting water levels for well {}...".format(
-          iwell, sid), end=' ')
 
-    # Add baro data to the WL time series.
-    barodata = baro_narr[[sid]]
-    barodata.rename(columns={sid: 'BP(m)'}, inplace=True)
-    barodata = barodata.resample('1H').asfreq()
-    barodata = barodata.interpolate(method='linear')
-
-    # Add Earth tide data to the WL time series.
-    etdata = earthtides[[sid]]
-    etdata = etdata[~etdata.index.duplicated()]
-    etdata = etdata.resample('1H').asfreq()
-    etdata = etdata.interpolate(method='linear')
-    etdata.rename(columns={sid: 'ET(nm/s**2)'}, inplace=True)
-
-    corr_data = barodata
-    corr_data = pd.merge(
-        corr_data, etdata, left_index=True, right_index=True, how='inner')
-
-    # ---- Correct the WL data with the BRF
-
-    # Get the BRF function.
-    brf_data = pd.read_csv(brf_fname, skip_blank_lines=False, header=14)
-
-    nlag = len(brf_data) - 1
-    ndat = len(corr_data)
-    dWL = np.empty(ndat) * np.nan
-
-    BP = scipy.signal.detrend(corr_data['BP(m)'])
-    M = np.empty((ndat - nlag, nlag + 1))
-    M[:, 0] = BP[nlag:]
-    A = brf_data['A'].copy()
-    A[A.isnull()] = 0
-    for i in range(1, nlag + 1):
-        M[:, i] = BP[nlag - i:-i]
-    dWL_BP = np.dot(M, A)
-
-    ET = scipy.signal.detrend(corr_data['ET(nm/s**2)'])
-    M = np.empty((ndat - nlag, nlag + 1))
-    M[:, 0] = ET[nlag:]
-    B = brf_data['B'].copy()
-    B[B.isnull()] = 0
-    for i in range(1, nlag + 1):
-        M[:, i] = ET[nlag - i:-i]
-    dWL_ET = np.dot(M, B) / 3.281
-
-    dWL[nlag:] = dWL_BP + dWL_ET
-
-    corr_data['dWL(m)'] = dWL
-
-    # ---- Plot the results
+    # ---- Get data for that well.
     sta_data = rsesq_reader.get_station_data(sid)
     sta_data.rename(columns={'Water Level (masl)': 'WL(masl)',
                              'Temperature (degC)': 'WT(degC)'},
                     inplace=True)
     sta_data.index.name = 'Date'
     sta_data = sta_data[~sta_data.index.duplicated()]
-    sta_data = pd.merge(sta_data, corr_data[['dWL(m)']],
-                        left_index=True, right_index=True,
-                        how='inner')
-    sta_data = sta_data[~sta_data.index.duplicated()]
 
-    WL = sta_data['WL(masl)'].copy()
-    WLcorr = sta_data['WL(masl)'] + sta_data['dWL(m)']
-    sta_data['WLcorr(masl)'] = sta_data['WL(masl)'] + sta_data['dWL(m)']
+    filename = '%s_%s.csv' % (rsesq_reader[sid]['ID'],
+                              rsesq_reader[sid]['Name'])
+    if sid in influenced:
+        print("{:>3d} - Saving water levels for influenced well {}...".format(
+              iwell, sid), end=' ')
+        dirname = osp.join(
+            osp.dirname(__file__), 'corrected_water_levels', 'not_corrected')
+    else:
+        # ---- Prepare the data for the correction
+        print("{:>3d} - Correcting water levels for well {}...".format(
+              iwell, sid), end=' ')
+        dirname = osp.join(osp.dirname(__file__), 'corrected_water_levels')
 
-    # ---- Save the data to file
-    filename = osp.join(
-        osp.dirname(__file__),
-        'corrected_water_levels',
-        '%s_%s.csv' % (rsesq_reader[sid]['ID'], rsesq_reader[sid]['Name'])
-        )
-    sta_data.to_csv(filename)
+        # Add baro data to the WL time series.
+        barodata = baro_narr[[sid]]
+        barodata.rename(columns={sid: 'BP(m)'}, inplace=True)
+        barodata = barodata.resample('1H').asfreq()
+        barodata = barodata.interpolate(method='linear')
 
-    with open(filename, 'r', encoding='utf8') as csvfile:
-        reader = list(csv.reader(csvfile, delimiter=','))
+        # Add Earth tide data to the WL time series.
+        etdata = earthtides[[sid]]
+        etdata = etdata[~etdata.index.duplicated()]
+        etdata = etdata.resample('1H').asfreq()
+        etdata = etdata.interpolate(method='linear')
+        etdata.rename(columns={sid: 'ET(nm/s**2)'}, inplace=True)
 
-    fcontent = [
-        ["Well Name", rsesq_reader[sid]['Name']],
-        ["Well ID", rsesq_reader[sid]['ID']],
-        ["Latitude", rsesq_reader[sid]['Latitude']],
-        ["Longitude", rsesq_reader[sid]['Longitude']],
-        ["Altitude", rsesq_reader[sid]['Elevation']],
-        ["Province", 'Qc'],
-        [],
-        ]
-    fcontent.extend(reader)
+        corr_data = barodata
+        corr_data = pd.merge(corr_data, etdata, left_index=True,
+                             right_index=True, how='inner')
 
-    with open(filename, 'w', encoding='utf8') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',', lineterminator='\n')
-        writer.writerows(fcontent)
+        # ---- Correct the WL data with the BRF
 
-    # ---- Plot the data.
-    if True:
-        # sta_data = sta_data.resample('1D').asfreq()
+        # Get the BRF function.
+        brf_data = pd.read_csv(brf_fname, skip_blank_lines=False, header=14)
 
+        nlag = len(brf_data) - 1
+        ndat = len(corr_data)
+        dWL = np.empty(ndat) * np.nan
+
+        # Calculate the effects of baro.
+        BP = scipy.signal.detrend(corr_data['BP(m)'])
+        M = np.empty((ndat - nlag, nlag + 1))
+        M[:, 0] = BP[nlag:]
+        A = brf_data['A'].copy()
+        A[A.isnull()] = 0
+        for i in range(1, nlag + 1):
+            M[:, i] = BP[nlag - i:-i]
+        dWL_BP = np.dot(M, A)
+
+        # Calculate the effects of earth tides.
+        ET = scipy.signal.detrend(corr_data['ET(nm/s**2)'])
+        M = np.empty((ndat - nlag, nlag + 1))
+        M[:, 0] = ET[nlag:]
+        B = brf_data['B'].copy()
+        B[B.isnull()] = 0
+        for i in range(1, nlag + 1):
+            M[:, i] = ET[nlag - i:-i]
+        dWL_ET = np.dot(M, B) / 3.281
+
+        # Calculate the total effect of baro and earth tides.
+        dWL[nlag:] = dWL_BP + dWL_ET
+        corr_data['dWL(m)'] = dWL
+
+        # Correct the water levels.
+        sta_data = pd.merge(sta_data, corr_data[['dWL(m)']],
+                            left_index=True, right_index=True,
+                            how='inner')
+        sta_data = sta_data[~sta_data.index.duplicated()]
+
+        WL = sta_data['WL(masl)'].copy()
+        WLcorr = sta_data['WL(masl)'] + sta_data['dWL(m)']
+        sta_data['WLcorr(masl)'] = sta_data['WL(masl)'] + sta_data['dWL(m)']
+
+        # ---- Plot the data.
         plt.ioff()
         fig, ax = plt.subplots()
         fig.set_size_inches(8, 4)
@@ -207,5 +192,28 @@ for sid in rsesq_reader.station_ids():
         fig.tight_layout()
         pdfpages.savefig(fig)
         plt.close('all')
+
+    # ---- Save the data to file
+    filename = osp.join(dirname, filename)
+    sta_data.to_csv(filename)
+
+    with open(filename, 'r', encoding='utf8') as csvfile:
+        reader = list(csv.reader(csvfile, delimiter=','))
+
+    fcontent = [
+        ["Well Name", rsesq_reader[sid]['Name']],
+        ["Well ID", rsesq_reader[sid]['ID']],
+        ["Latitude", rsesq_reader[sid]['Latitude']],
+        ["Longitude", rsesq_reader[sid]['Longitude']],
+        ["Altitude", rsesq_reader[sid]['Elevation']],
+        ["Province", 'Qc'],
+        [],
+        ]
+    fcontent.extend(reader)
+
+    with open(filename, 'w', encoding='utf8') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', lineterminator='\n')
+        writer.writerows(fcontent)
+
     print('done')
 pdfpages.close()
