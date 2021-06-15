d0f05a1180e01d2195982eb07938628765e83a88
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2021-06-15 13:58:46 -0400

    Update the folder where png image files are saved

--- meteo_stations_vs_grid/compare_meteo_grid.py
+++ meteo_stations_vs_grid/compare_meteo_grid.py
@@ -354,7 +354,7 @@ for period in periods:
     with PdfPages(osp.join(dirname, filename)) as pdf:
         for i, fig in enumerate(figures):
             filename = 'precip_grid_vs_station_{}_page{}.png'.format(period, i)
-            fig.savefig(osp.join(dirname, filename), dpi=300)
+            fig.savefig(osp.join(dirname, 'figures_png', filename), dpi=300)
             pdf.savefig(fig)
 plt.close('all')
 
@@ -476,7 +476,7 @@ filename = 'fdp_precip_grid_vs_station.pdf'
 with PdfPages(osp.join(dirname, filename)) as pdf:
     for i, fig in enumerate(figures):
         filename = 'fdp_precip_grid_vs_station_page{}.png'.format(i)
-        fig.savefig(osp.join(dirname, filename), dpi=300)
+        fig.savefig(osp.join(dirname, 'figures_png', filename), dpi=300)
         pdf.savefig(fig)
 plt.close('all')
 
@@ -643,6 +643,6 @@ for var in ['tamin', 'tamax']:
     with PdfPages(osp.join(dirname, filename)) as pdf:
         for i, fig in enumerate(figures):
             filename = '{}_grid_vs_station_daily_page{}.png'.format(var, i)
-            fig.savefig(osp.join(dirname, filename), dpi=300)
+            fig.savefig(osp.join(dirname, 'figures_png', filename), dpi=300)
             pdf.savefig(fig)
 plt.close('all')
