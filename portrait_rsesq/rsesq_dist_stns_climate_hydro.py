7a95c6979a920fc0a454c58db41172477d1e36ff
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2021-09-22 14:00:27 -0400

    Update code to produce distance to stations

--- portrait_rsesq/rsesq_dist_stns_climate_hydro.py
+++ portrait_rsesq/rsesq_dist_stns_climate_hydro.py
@@ -6,12 +6,75 @@
 # Licensed under the terms of the MIT License.
 # -----------------------------------------------------------------------------
 
+import os.path as osp
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.transforms as transforms
-from readers import (MDDELCC_RSESQ_Reader,
-                     MDDELCC_CEHQ_Reader,
-                     EC_Climate_Reader)
+
+workdir = "D:/Projets/pacc-inrs/portrait_rsesq"
+
+
+def read_rsesq_coord():
+    rsesq_data = np.load(
+        osp.join(workdir, 'mddelcc_rsesq_database.npy'),
+        allow_pickle=True).item()
+
+    stn_coord = pd.DataFrame([], columns=['lat_dd', 'lon_dd'])
+    for stn_id, stn_data in rsesq_data.items():
+        stn_coord.loc[stn_id, 'lat_dd'] = stn_data['Latitude']
+        stn_coord.loc[stn_id, 'lon_dd'] = stn_data['Longitude']
+    stn_coord['lat_dd'] = stn_coord['lat_dd'].astype('float')
+    stn_coord['lon_dd'] = stn_coord['lon_dd'].astype('float')
+
+    return stn_coord
+
+
+def read_climstn_coord():
+    climstn_data = pd.read_csv(
+        osp.join(workdir, 'Station Inventory EN.csv'), skiprows=2,
+        index_col='Name',
+        usecols=['Name',
+                 'Latitude (Decimal Degrees)',
+                 'Longitude (Decimal Degrees)',
+                 'HLY Last Year',
+                 'DLY Last Year',
+                 'MLY Last Year']
+        )
+    climstn_data = climstn_data.rename(columns={
+        'Latitude (Decimal Degrees)': 'lat_dd',
+        'Longitude (Decimal Degrees)': 'lon_dd'})
+    climstn_data['lat_dd'] = climstn_data['lat_dd'].astype('float')
+    climstn_data['lon_dd'] = climstn_data['lon_dd'].astype('float')
+
+    climstn_data['active'] = (
+        (climstn_data['DLY Last Year'] == 2021) |
+        (climstn_data['MLY Last Year'] == 2021) |
+        (climstn_data['HLY Last Year'] == 2021)
+        )
+
+    climstn_data = climstn_data.drop(
+        ['HLY Last Year', 'DLY Last Year', 'MLY Last Year'], axis=1)
+
+    return climstn_data
+
+
+def read_cehq_coord():
+    cehq_data = np.load(
+        osp.join(workdir, 'mddelcc_cehq_database.npy'),
+        allow_pickle=True).item()
+
+    stn_coord = pd.DataFrame([], columns=['lat_dd', 'lon_dd'])
+    for stn_id, stn_data in cehq_data.items():
+        stn_coord.loc[stn_id, 'lat_dd'] = float(stn_data['Latitude'])
+        stn_coord.loc[stn_id, 'lon_dd'] = float(stn_data['Longitude'])
+        stn_coord.loc[stn_id, 'active'] = (
+            stn_data['Status'] == 'Station ouverte')
+
+    stn_coord['lat_dd'] = stn_coord['lat_dd'].astype('float')
+    stn_coord['lon_dd'] = stn_coord['lon_dd'].astype('float')
+
+    return stn_coord
 
 
 def find_closest_location(lat1, lon1, lat2, lon2):
@@ -35,31 +98,30 @@ def calc_rsesq_dist_to_climate_and_hydro():
     Compute the distance between each piezometric station of the RSESQ
     and the nearest climatic and hydrometric station.
     """
-    reader_eccc = EC_Climate_Reader()
-    climstns = reader_eccc.stations(active=True, prov='QC')
-    lat2 = np.radians([float(stn['Latitude']) for stn in climstns])
-    lon2 = np.radians([float(stn['Longitude']) for stn in climstns])
+    climstn_data = read_climstn_coord()
+    lat2 = np.radians(
+        climstn_data[climstn_data['active'] == True]['lat_dd'].values)
+    lon2 = np.radians(
+        climstn_data[climstn_data['active'] == True]['lon_dd'].values)
     dist_to_climstn = []
+    
+    cehq_coord = read_cehq_coord()
+    lat3 = np.radians(
+        cehq_coord[cehq_coord['active'] == True]['lat_dd'].values)
 
-    reader_cehq = MDDELCC_CEHQ_Reader()
-    hydstns = reader_cehq.stations(active=True)
-    lat3 = np.radians([float(stn['Latitude']) for stn in hydstns])
-    lon3 = np.radians([float(stn['Longitude']) for stn in hydstns])
+    lon3 = np.radians(
+        cehq_coord[cehq_coord['active'] == True]['lon_dd'].values)
     dist_to_hydstn = []
 
-    reader_rsesq = MDDELCC_RSESQ_Reader()
-    rsesq_stns = reader_rsesq.stations()
-    for stn in rsesq_stns:
-        if 'Year' not in list(stn.keys()):
-            continue
-
-        lat1 = np.radians(float(stn['Latitude']))
-        lon1 = np.radians(float(stn['Longitude']))
+    rsesq_coord = read_rsesq_coord()
+    for stn_idx, stn_data in rsesq_coord.iterrows():
+        lat1 = np.radians(stn_data['lat_dd'])
+        lon1 = np.radians(stn_data['lon_dd'])
 
         dist_to_climstn.append(find_closest_location(lat1, lon1, lat2, lon2))
         dist_to_hydstn.append(find_closest_location(lat1, lon1, lat3, lon3))
 
-    return rsesq_stns, np.array(dist_to_climstn), np.array(dist_to_hydstn)
+    return np.array(dist_to_climstn), np.array(dist_to_hydstn)
 
 
 def plot_bar_diagram(dist1, dist2):
@@ -108,8 +170,7 @@ def plot_bar_diagram(dist1, dist2):
                    fontsize=16, labelpad=25)
 
     # Define the axe ticks and labels.
-
-    ax.axis(ymin=-300, ymax=300)
+    ax.axis(ymin=-320, ymax=320)
     ypos = np.arange(-300, 301, 100)
     ax.set_yticks(ypos)
     ax.set_yticklabels(np.abs(ypos))
@@ -120,7 +181,6 @@ def plot_bar_diagram(dist1, dist2):
     ax2.set_xticklabels(["\u2264"+str(v) for v in bins])
 
     # Plot text values over the barplot.
-
     for x, value, value2 in zip(xpos, values, values2):
         offset = transforms.ScaledTranslation(0, 2/72, fig.dpi_scale_trans)
         text = "%d\n(%d%%)" % (value, value/len(dist1)*100)
@@ -133,14 +193,13 @@ def plot_bar_diagram(dist1, dist2):
                  transform=ax.transData+offset)
 
     # Show and save figure.
-
     plt.tight_layout()
     plt.show(block=False)
-    fig.savefig('rsesq_dist_stns_climate_hydro.pdf')
+    fig.savefig(osp.join(workdir, 'rsesq_dist_stns_climate_hydro.pdf'))
 
 
 if __name__ == "__main__":
-    results = calc_rsesq_dist_to_climate_and_hydro()
-    rsesq_stns, dist_to_climstn, dist_to_hydstn = results
+    cehq_coord = read_cehq_coord()
+    dist_to_climstn, dist_to_hydstn = calc_rsesq_dist_to_climate_and_hydro()
     plt.close('all')
     plot_bar_diagram(dist_to_climstn, dist_to_hydstn)
